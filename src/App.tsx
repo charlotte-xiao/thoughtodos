631176ba301e83784ab0acccf405bbb78aa3a187
xiao sa <sa.xiao@thoughtworks.com>
2022-10-27 15:00:21 +0800

    <refactor, xs, [E-3]>: use styled-components replace App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import './App.css';
+import styled from "styled-components";
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Container = styled.div`
+ text-align: center;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Thoughtodos!</h1>
-      </header>
-    </div>
-  );
+    return (
+        <Container>
+            <Header>
+                <h1>Thoughtodos!</h1>
+            </Header>
+        </Container>
+    );
 }
 
 export default App;
