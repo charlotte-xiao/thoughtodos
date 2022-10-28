15ce2f9f1534d976bd5ea4ca49cdfd8e3ae7cde0
xiao sa <sa.xiao@thoughtworks.com>
2022-10-28 17:33:54 +0800

    <feat, xs, [E-3]>: add CheckBox isCompleted CSS

--- src/component/TaskItem/index.tsx
+++ src/component/TaskItem/index.tsx
@@ -3,23 +3,33 @@ import styled from "styled-components";
 
 const Item = styled.div`
   line-height: 4rem;
-  padding: 0rem 1rem;
+  padding: 0 1rem;
   display: flex;
   flex-direction: row;
-
   .is_completed {
     color: #E2E8F0;;
     text-decoration:line-through;
     
   }
-  
 `
+
+const Info = styled.div`
+  padding-left: 1.5rem;
+  text-align: left;
+  flex: 1;
+`
+
 export default class TaskItem extends React.Component<any, any> {
 
     render() {
         return (
             <Item>
-                <div className={this.props.task.isCompleted ? 'is_completed' : ''}>{this.props.task.name}</div>
+                <input
+                    type="checkbox"
+                    defaultChecked={this.props.task.isCompleted}
+                    className="input-checkbox"
+                />
+                <Info className={this.props.task.isCompleted ? 'is_completed' : ''}>{this.props.task.name}</Info>
             </Item>
 
         )
