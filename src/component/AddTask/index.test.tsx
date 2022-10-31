94172f14e05a097e33c8a0d2d44a4fa35b54ac8a
xiao sa <sa.xiao@thoughtworks.com>
2022-10-31 14:31:55 +0800

    <feat, xs, [E-3]>: add input and button for AddTask component

--- src/component/AddTask/index.test.tsx
+++ src/component/AddTask/index.test.tsx
@@ -1,14 +1,31 @@
 import {render, screen} from '@testing-library/react';
 import React from 'react';
 import AddTask from "./index";
+import userEvent from "@testing-library/user-event";
+import Task from "../../models/Task";
 
 describe('AddTask Test', () => {
-    test('should render add button', () => {
+    test('should render add button and input', () => {
 
         render(<AddTask toggleTaskList={jest.fn()}></AddTask>);
 
-        expect(screen.getByRole('button', { name: 'Add Task' })).toBeInTheDocument();
+        expect(screen.getByRole('button', {name: 'Add Task'})).toBeInTheDocument();
+        expect(screen.getByRole('textbox')).toBeInTheDocument();
 
+    });
+
+    test('should execute ToggleTaskList when click add button', () => {
+        const mockToggleTaskList = jest.fn();
+        const mockNewTask: Task = {
+            name: 'new Task',
+            isCompleted: false,
+        }
+
+        render(<AddTask toggleTaskList={mockToggleTaskList}></AddTask>);
+        userEvent.type(screen.getByRole('textbox'), mockNewTask.name);
+        userEvent.click(screen.getByRole('button', {name: 'Add Task'}));
+
+        expect(mockToggleTaskList).toHaveBeenLastCalledWith(mockNewTask);
     });
 });
 
--- src/component/AddTask/index.tsx
+++ src/component/AddTask/index.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import Task from "../../models/Task";
 
 export default class AddTask extends React.Component<any, any> {
 
+    constructor(props: never) {
+        super(props);
+        this.state = {
+            name: ''
+        }
+    }
+
     addNewTask = () => {
-        const tempTask: Task = {
-            name: "temp",
+        const newTask: Task = {
+            name: this.state.name,
             isCompleted: false,
         }
-        this.props.toggleTaskList(tempTask);
+        this.props.toggleTaskList(newTask);
+    }
+
+    toggleTaskName = (event: ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            name: event.target.value
+        })
     }
 
     render() {
-        return ( <input type="button" onClick={this.addNewTask} value = "Add Task"/>);
+        return (
+            <>
+                <input type="text" value={this.state.name} onChange={this.toggleTaskName}/>
+                <input type="button" onClick={this.addNewTask} value="Add Task"/>
+            </>);
     }
+
 }
