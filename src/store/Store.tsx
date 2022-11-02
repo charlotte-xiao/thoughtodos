45901684f631228067f13ded0333c5d58ca9347d
xiao sa <sa.xiao@thoughtworks.com>
2022-11-02 16:14:53 +0800

    <feat, xs, [E-4]>: create redux store

---
+++ src/store/Store.tsx
@@ -0,0 +1,24 @@
+import {legacy_createStore as createStore} from 'redux'
+import TaskService from "../service/TaskService";
+import TaskList from "../models/TaskList";
+import defaultTaskLit from "../default/defaultTaskList.json"
+import {ACTION_TYPE} from "../constants/ActionType";
+import Task from "../models/Task";
+import {Action} from "@reduxjs/toolkit";
+
+export type TaskReducer = {
+    (state: TaskList | undefined, actionParams: Action<TaskAction>): TaskList
+}
+
+export type TaskAction = {
+    actionType: ACTION_TYPE,
+    task: Task
+}
+
+const taskReducer: TaskReducer = (state = defaultTaskLit, actionParams) => {
+    const taskService = new TaskService();
+    return taskService.executeStrategy(actionParams.type.actionType, state, actionParams.type.task);
+};
+
+// Todo: OUT OF DATE API
+export const store = createStore(taskReducer);
