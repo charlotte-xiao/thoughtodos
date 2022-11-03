3f59ae561a3ac31c9da22d4bc7c534fcd2909191
xiao sa <sa.xiao@thoughtworks.com>
2022-11-03 16:11:57 +0800

    <test, xs, [E-5]>: add test for task service

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    // 自动清除 Mock
-    clearMocks: true,
-    // 覆盖率
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageProvider: "v8",
-    // 覆盖路径
-    collectCoverageFrom: [
-        "<rootDir>/src/component/**/*.{tsx,ts}",
-        "<rootDir>/src/pages/**/*.{tsx,ts}",
-    ],
+  // tsc 转译
+  preset: 'ts-jest',
+  // 自动清除 Mock
+  clearMocks: true,
+  // 覆盖率
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  // 覆盖路径
+  collectCoverageFrom: [
+    "<rootDir>/src/component/**/*.{tsx,ts}",
+    "<rootDir>/src/pages/**/*.{tsx,ts}",
+    "<rootDir>/src/service/**/*.{tsx,ts}",
+  ],
+  testEnvironment: "jsdom",
 };
