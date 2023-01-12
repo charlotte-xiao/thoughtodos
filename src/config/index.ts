2ffe18782283018667a9876fe95b2c29d3f14837
Xiao Sa <sa.xiao@thoughtworks.com>
2023-01-12 16:25:12 +0800

    <fix, xs, [E-14]>: fix url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
 export const FRONTEND_URL = "http://www.xiaostudy.cn";
 
-export const BACKEND_URL = "http://localhost:9000";
-//  "https://service-q0uypjk0-1302925073.gz.apigw.tencentcs.com/release";
+export const BACKEND_URL = "http://www.xiaostudy.cn:3000";
 
 export const GITHUB_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=bad5d978cbbb678fb88c&redirect_uri=${FRONTEND_URL}/login`;
