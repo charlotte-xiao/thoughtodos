ffbec0cd7ff30d677e625bad997beaf2e23708ef
Xiao Sa <sa.xiao@thoughtworks.com>
2023-01-12 09:39:15 +0800

    <refactor, xs, [E-14]>: update url

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,9 +1,9 @@
 import { axiosInstance } from "./interceptor";
-import { BASE_URL } from "../constants/Commom";
+import { BACKEND_URL } from "../config";
 
 const loginGithub = async (code: string) => {
   const { data } = await axiosInstance.get(
-    `${BASE_URL}/user/login/github?code=${code}`
+    `${BACKEND_URL}/user/login/github?code=${code}`
   );
   return data;
 };
